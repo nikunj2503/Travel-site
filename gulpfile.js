62165eff297cd7ad31e83e30d0922565acb50b83
Nikunj <nikunj.patel2548@gmail.com>
2017-03-29 16:31:29 -0700

    Completed gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ require('./Gulp/tasks/watch');
 require('./Gulp/tasks/sprites');
 require('./Gulp/tasks/scripts');
 require('./Gulp/tasks/modernizr');
-
+require('./Gulp/tasks/build');
     
         
 
